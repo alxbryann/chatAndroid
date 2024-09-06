29ee44513ac7f1d0a5f54f617ca1b814bcebcc79
alxbryann <bryanalexanderbogota@gmail.com>
2024-09-06 17:49:58 -0500

    correct function

--- app/src/main/java/com/alxbryann/mi_hola_mundo/MainActivity.java
+++ app/src/main/java/com/alxbryann/mi_hola_mundo/MainActivity.java
@@ -3,6 +3,7 @@ package com.alxbryann.mi_hola_mundo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -14,13 +15,18 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
 TextView mensajeTextView;
 EditText textoDeEntrada;
+Button enviar;
+
 
     public void buttonPress(View view){
 
         Log.i("Info: ", "Boton presionado");
+        textoDeEntrada.setVisibility(View.INVISIBLE);
         mensajeTextView.setText(textoDeEntrada.getText());
+        enviar.setVisibility(View.INVISIBLE);
 
 
     }
@@ -36,7 +42,8 @@ EditText textoDeEntrada;
         });
 
         mensajeTextView = findViewById(R.id.textView);
-        mensajeTextView.setText("Este mensaje va a cambiar :)");
+        mensajeTextView.setText("Esperando mensaje :)");
         textoDeEntrada = findViewById(R.id.editText);
+        enviar = findViewById(R.id.button);
     }
 }
